ab7eca92ba42e2be084cd1e1986eab91ff1c54a6
albertnanda19 <albertnanda1902@gmail.com>
2025-03-15 21:04:42 +0800

    first commit

---
+++ src/main/java/com/sehati/backend_sehati/BackendSehatiApplication.java
@@ -0,0 +1,13 @@
+package com.sehati.backend_sehati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSehatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSehatiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sehati/backend_sehati/BackendSehatiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sehati.backend_sehati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSehatiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
